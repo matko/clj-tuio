ef30774854eec70a2d2cc3f71f55dcd0b3fb0dd0
Matthijs van Otterdijk <matko@pievolution.org>
2013-04-24 11:18:41 +0200

    initial tuio protocol prototyping

--- src/clj_tuio/core.clj
+++ src/clj_tuio/core.clj
@@ -1,6 +1,36 @@
-(ns clj-tuio.core)
+(ns clj-tuio.core
+  (:import (com.illposed.osc OSCPortIn OSCListener OSCMessage))
+  (:use clojure.tools.logging))
+(def tuio-address "/tuio/2Dcur")
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defrecord Pointer
+    [x y alive])
+
+(def printer (agent nil))
+
+(let [repl-out *out*]
+  (defn repl-print [& args]
+    (send printer (fn [_]
+                    (binding [*out* repl-out]
+                      (apply println args))))
+    nil))
+
+(defn- listener []
+(proxy [OSCListener] []
+  (acceptMessage [time message]
+    (let [arguments (.getArguments message)]
+      (repl-print time (aget arguments 0))))))
+
+(defn- receiver [port]
+  (OSCPortIn. port))
+
+(defn start [port]
+  (doto (receiver port)
+       (.addListener ,,, tuio-address (listener))
+       (.startListening ,,,)))
+
+(defn stop [receiver]
+  (doto receiver
+      (.stopListening)
+      (.close))
+  nil)
