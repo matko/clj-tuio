fbe8a77f5af1bf7da4d0ebbd8fef482b1c3963c2
jlicht <jlicht@aerix.nl>
2013-04-24 18:58:22 +0200

    Removed complex solution, and replaced it with a simple, for-based one

--- src/clj_tuio/pointer.clj
+++ src/clj_tuio/pointer.clj
@@ -14,18 +14,32 @@
 (defn add! [id pointer]
   (swap! pointers #(assoc % id pointer)))
 
-(defn- point-difference [m coll]
-  (clojure.set/difference
-   (set (keys m)) (set coll)))
+;; TESTAMENT TO STUPIDITY
+;; (defn- point-difference [m coll]
+;;   (clojure.set/difference
+;;    (set (keys m)) (set coll)))
 
-(defn remove-selected! [alive-keys f]
-  (swap! pointers
-         #(let [current-pointers %
-               difference (point-difference current-pointers alive-keys)]
-           (map (partial apply f)
-                (filter (comp difference key)
-                        current-pointers))
-           (apply dissoc current-pointers difference))))
+;; (defn remove-selected! [alive-keys f]
+;;   (swap! pointers
+;;          #(let [current-pointers %
+;;                difference (point-difference current-pointers alive-keys)]
+;;            (map (partial apply f)
+;;                 (filter (comp difference key)
+;;                         current-pointers))
+;;            (apply dissoc current-pointers difference))))
+
+(defn- remove-some [m s f]
+  (apply dissoc m
+         (for [[k v] m
+               :when (not (s k))]
+           (do
+             (f k v)
+             k))))
+
+(defn remove-some!
+  "Calls fn f with all [id , point] entries for which id is not present in set s"
+  [s f]
+  (swap! pointers #(remove-some % s f)))
 
 (defn retrieve [id]
   (get @pointers id))
