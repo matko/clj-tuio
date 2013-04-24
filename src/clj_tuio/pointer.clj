68b72b6721a9d195b0f8dc4617dad0a87646ce37
Matthijs van Otterdijk <matko@pievolution.org>
2013-04-24 11:56:18 +0200

    pointer utilities

---
+++ src/clj_tuio/pointer.clj
@@ -0,0 +1,20 @@
+(ns clj-tuio.pointer
+  (:import (com.illposed.osc OSCPortIn OSCListener OSCMessage))
+  (:use clojure.tools.logging))
+
+(defrecord Pointer
+    [x y])
+
+(defonce pointers (atom {}))
+
+(defn alive? [id]
+  (contains? @pointers id))
+
+(defn add [id pointer]
+  (swap! pointers #(assoc % id pointer)))
+
+(defn remove [id]
+  (swap! pointers #(dissoc % id)))
+
+(defn pointer [id]
+  (get @pointers id))
